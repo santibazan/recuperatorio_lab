0b544ece23bd2a30b6e02844cb155f31ecc1a870
santi bazan <santiago.n.bazan@outlook.com>
2023-10-19 09:24:45 -0300

    recuperatorio parcial Bazan

--- CalculadoraIVA_SantiagoBazan.java
+++ CalculadoraIVA_SantiagoBazan.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class CalculadoraI_SantiagoBazan {
+public class CalculadoraIVA_SantiagoBazan {
     public static void main(String[] args){
         /*Definimos las variables numericas y para las opciones*/
         float precioProd, iva, resultado;
@@ -14,7 +14,7 @@ public class CalculadoraI_SantiagoBazan {
         System.out.println("Ingrese el valor del IVA: ");
         iva=leer.nextInt();
 
-        /*Le pedimos al usuario que ingrese la operacion que desee*/
+        /*Le pedimos al usuario que ingrese la operacion que desee realizar*/
         do {
             do {
                 System.out.println("Menu de opciones.");
@@ -25,35 +25,46 @@ public class CalculadoraI_SantiagoBazan {
                 op=leer.nextInt();
             }while (op != 1 && op != 2 && op != 3 && op != 4);
             switch (op){
+                /*Si elusuario ingresa la opcion 1 se realizara la suma*/
                 case 1:
                     System.out.println("Ingreso a la opcion para sumar el IVA.");
+                    /*Se realiza la suma*/
                     resultado=(precioProd*iva/100)+precioProd;
                     System.out.println("La suma entre el IVA y el precio del producto es "+resultado);
                     break;
+                /*Si elusuario ingresa la opcion 2 se realizara la resta*/
                 case 2:
                     System.out.println("Ingreso a la opcion para restar el IVA.");
+                    /*Se realiza la resta*/
                     resultado=precioProd-(precioProd*iva/100);
                     System.out.println("La resta entre el precio del producto y el IVA es "+resultado);
                     break;
+                /*Si elusuario ingresa la opcion 3 se realizara la multiplicacion*/
                 case 3:
                     System.out.println("Ingreso a la opcion para multiplicar el IVA.");
+                    /*Se realiza la multiplicacion*/
                     resultado=(precioProd*iva/100)*precioProd;
                     System.out.println("La multiplicacion entre el IVA y el precio del producto es "+resultado);
                     break;
+                /*Si elusuario ingresa la opcion 4 se realizara la division*/
                 case 4:
                     System.out.println("Ingreso a la opcion para dividir el IVA.");
+                    /*Se evalua si en la division no hay ceros*/
                     if (precioProd == 0 || iva == 0){
                         System.out.println("No se puede calcular la division ya que hay ceros");
                         break;
                     }else{
+                        /*Se realiza la division*/
                         resultado=precioProd/(precioProd*iva/100);
                         System.out.println("La division entre el precio del producto y el IVA es "+resultado);
                     }
                     break;
             }
+            /*Se le pide al usuario si quiere seguir en la calculadora o si desea salir*/
             System.out.println("Si desea realizar otra operacion presione cualquier numero, si desea salir del programa presione 0");
             op2 = leer.nextInt();
         }while (op2 != 0);
+        /*Nos despedimos del usuario*/
         System.out.println("Saliendo del programa, muchas gracias");
 
     }
